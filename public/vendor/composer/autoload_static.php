2b5aa831ae41fbefd4ba3bdb12dadfe48a14ef8f
EdwardBock <edward.bock@palasthotel.de>
2020-11-27 13:30:47 +0100

    updated autoload

--- public/vendor/composer/autoload_static.php
+++ public/vendor/composer/autoload_static.php
@@ -31,21 +31,25 @@ class ComposerStaticInit60014a30e971434c4f80508881fef88d
 
     public static $classMap = array (
         'Palasthotel\\ProLitteris\\API' => __DIR__ . '/../..' . '/classes/API.php',
+        'Palasthotel\\ProLitteris\\Assets' => __DIR__ . '/../..' . '/classes/Assets.php',
         'Palasthotel\\ProLitteris\\DashboardWidget' => __DIR__ . '/../..' . '/classes/DashboardWidget.php',
         'Palasthotel\\ProLitteris\\Database' => __DIR__ . '/../..' . '/classes/Database.php',
-        'Palasthotel\\ProLitteris\\MetaBox' => __DIR__ . '/../..' . '/classes/MetaBox.php',
+        'Palasthotel\\ProLitteris\\Gutenberg' => __DIR__ . '/../..' . '/classes/Gutenberg.php',
+        'Palasthotel\\ProLitteris\\MessageUtils' => __DIR__ . '/../..' . '/classes/MessageUtils.php',
         'Palasthotel\\ProLitteris\\Model\\FetchPixelsResponse' => __DIR__ . '/../..' . '/classes/Model/FetchPixelsResponse.php',
+        'Palasthotel\\ProLitteris\\Model\\Message' => __DIR__ . '/../..' . '/classes/Model/Message.php',
         'Palasthotel\\ProLitteris\\Model\\Pixel' => __DIR__ . '/../..' . '/classes/Model/Pixel.php',
+        'Palasthotel\\ProLitteris\\Model\\PreventPixelAssign' => __DIR__ . '/../..' . '/classes/Model/PreventPixelAssign.php',
+        'Palasthotel\\ProLitteris\\Model\\PushMessageResponse' => __DIR__ . '/../..' . '/classes/Model/PushMessageResponse.php',
         'Palasthotel\\ProLitteris\\Model\\_BaseAPIResponse' => __DIR__ . '/../..' . '/classes/Model/_BaseAPIResponse.php',
-        'Palasthotel\\ProLitteris\\NoParticipantException' => __DIR__ . '/../..' . '/classes/NoParticipantException.php',
         'Palasthotel\\ProLitteris\\Options' => __DIR__ . '/../..' . '/classes/Options.php',
         'Palasthotel\\ProLitteris\\Post' => __DIR__ . '/../..' . '/classes/Post.php',
         'Palasthotel\\ProLitteris\\PostsTable' => __DIR__ . '/../..' . '/classes/PostsTable.php',
         'Palasthotel\\ProLitteris\\Repository' => __DIR__ . '/../..' . '/classes/Repository.php',
         'Palasthotel\\ProLitteris\\Schedule' => __DIR__ . '/../..' . '/classes/Schedule.php',
-        'Palasthotel\\ProLitteris\\Service' => __DIR__ . '/../..' . '/classes/Service.php',
         'Palasthotel\\ProLitteris\\TrackingPixel' => __DIR__ . '/../..' . '/classes/TrackingPixel.php',
         'Palasthotel\\ProLitteris\\User' => __DIR__ . '/../..' . '/classes/User.php',
+        'Palasthotel\\ProLitteris\\WP_REST' => __DIR__ . '/../..' . '/classes/WP_REST.php',
         'Palasthotel\\ProLitteris\\_Component' => __DIR__ . '/../..' . '/classes/_Component.php',
     );
 
