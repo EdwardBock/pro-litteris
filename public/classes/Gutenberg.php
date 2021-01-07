f76016069ce97669a6f96b7b314ddce1b2350dcf
EdwardBock <edward.bock@palasthotel.de>
2021-01-07 07:01:43 +0100

    only enqueue script if needed

--- public/classes/Gutenberg.php
+++ public/classes/Gutenberg.php
@@ -8,7 +8,9 @@ class Gutenberg extends _Component {
 	public function onCreate() {
 		parent::onCreate();
 		add_action( 'enqueue_block_editor_assets', function () {
-			$this->plugin->assets->enqueueGutenberg();
+			if(in_array(get_post_type(), $this->plugin->pixel->enabledPostTypes())){
+				$this->plugin->assets->enqueueGutenberg();
+			}
 		});
 	}
 }
