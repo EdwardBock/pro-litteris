b6b6a8b55af9e04025c123457841937a9fc5a377
EdwardBock <edward.bock@palasthotel.de>
2021-04-08 12:25:24 +0200

    for later usage use-blocks images hooks

---
+++ src/scripts/hooks/use-blocks.js
@@ -0,0 +1,40 @@
+import flatten from "lodash/flatten";
+import {useSelect} from "@wordpress/data";
+import {useEffect, useState} from "@wordpress/element";
+
+const useBlocks = (deps = [])=> useSelect( select =>{
+    const store = select('core/block-editor');
+    return store ? store.getBlocks() : [];
+}, deps);
+
+const useAttachmentRecords = (ids)=> useSelect(select => {
+    const store = select('core');
+    return store ? store.getEntityRecords('postType','attachment', {include:ids}) : [];
+}, [ids]);
+
+export const useImages = ()=>{
+    const blocks = useBlocks();
+
+    const validImageBlocks = blocks.filter(b =>{
+        if(b.name !== "core/image") return false;
+        if(typeof b.attributes !== typeof [] || typeof b.attributes.id === typeof undefined) return false;
+        return true;
+    });
+    const validGalleryBlocks = blocks.filter(g=>{
+        if(g.name !== "core/gallery") return false;
+        if(typeof g.attributes !== typeof [] || typeof g.attributes.ids !== typeof []) return false;
+        return true;
+    });
+
+    const imageIds = [
+        ...new Set([...validImageBlocks.map(b=>b.attributes.id), ...flatten(validGalleryBlocks.map(b=>b.attributes.ids))]),
+    ];
+
+    //const images = [...validImageBlocks.map(b=>b.attributes), ...flatten(validGalleryBlocks.map(b=>b.attributes.images))];
+
+    const images = useAttachmentRecords(imageIds) ?? [];
+
+    console.debug("images", images)
+
+    return images;
+}
