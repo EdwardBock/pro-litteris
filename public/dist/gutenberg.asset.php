87d0fe11b3797bebe7d80132e451e7ee148aedd4
EdwardBock <edward.bock@palasthotel.de>
2023-04-20 15:09:20 +0200

    package updates

--- public/dist/gutenberg.asset.php
+++ public/dist/gutenberg.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '5c83127a7b0dc32e66e2');
+<?php return array('dependencies' => array('wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-element', 'wp-plugins'), 'version' => '678d9012dc19bf9de8db');
