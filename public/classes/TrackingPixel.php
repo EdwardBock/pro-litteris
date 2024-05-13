836cbd81796aa363498bf2b016af58dc03ef473a
Edward <edward.bock@palasthotel.de>
2024-05-13 06:34:12 +0200

    move pixel from content to footer

--- public/classes/TrackingPixel.php
+++ public/classes/TrackingPixel.php
@@ -13,9 +13,8 @@ class TrackingPixel extends _Component {
 
 	public function onCreate() {
 		parent::onCreate();
-		add_action('wp_head', [$this, 'head']);
-		add_filter( 'the_content', array( $this, 'add_pixel' ) );
-		add_action( 'amp_post_template_footer', array( $this, 'amp_post_template_footer' ) );
+		add_action( 'wp_head', [$this, 'head'] );
+		add_filter( 'wp_footer', array( $this, 'footer' ) );
 	}
 
 	/**
@@ -36,14 +35,6 @@ class TrackingPixel extends _Component {
 		return in_array($postType, $this->enabledPostTypes());
 	}
 
-	/**
-	 * request is on amp page
-	 * @return bool
-	 */
-	private function isAmp() {
-		return ( function_exists( 'is_amp_endpoint' ) && is_single() && is_amp_endpoint() );
-	}
-
 	public function head(){
 		?>
 		<!-- pro-litteris -->
@@ -52,58 +43,26 @@ class TrackingPixel extends _Component {
 	}
 
 	/**
-	 * Add pixel to content if exists
+	 * Add pixel to footer if exists
 	 *
-	 * @param string $content
-	 *
-	 * @return string
 	 */
-	public function add_pixel( $content ) {
-
-		if ( $this->isAmp() ) {
-			return $content;
-		}
-
-		if(!$this->isEnabled(get_post_type())){
-			return $content;
-		}
-
-		if(!apply_filters(Plugin::FILTER_RENDER_PIXEL, true)){
-		    return $content;
-        }
-
-		$pixel = $this->plugin->repository->getPostPixel(get_the_ID());
-
-		if(!($pixel instanceof Pixel)){
-			return $content;
-		}
-
-		return $content . '<img src="' . $pixel->toUrl() . '" height="1" width="1" border="0" class="pro-litteris-pixel" />';
-
-	}
-
-	public function amp_post_template_footer() {
+	public function footer() {
 
 		if(!$this->isEnabled(get_post_type())){
 			return;
 		}
 
-        if(!apply_filters(Plugin::FILTER_RENDER_PIXEL, true)){
-            return;
+		if(!apply_filters(Plugin::FILTER_RENDER_PIXEL, true)){
+		    return;
         }
 
-        $pixel = $this->plugin->repository->getPostPixel(get_the_ID());
+		$pixel = $this->plugin->repository->getPostPixel(get_the_ID());
 
-        if($pixel instanceof Pixel){
-			?>
-			<amp-pixel
-				class="pro-litteris-pixel"
-				src="<?= $pixel->toUrl(); ?> "
-				layout="nodisplay"
-			></amp-pixel>
-			<?php
+		if(!($pixel instanceof Pixel)){
+			return;
 		}
 
+		echo '<img src="' . $pixel->toUrl() . '" height="1" width="1" border="0" class="pro-litteris-pixel" />';
 	}
 
 
